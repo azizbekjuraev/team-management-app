4a201e6c216cf53a2e27898dc23af4d6b79e4cfc
AZIZBEKJURAEV <ajuraev98@mail.ru>
2023-01-17 21:48:17 +0500

    bootstrap installed, chnaged some style, added condition to Employees component

--- src/components/Employees.jsx
+++ src/components/Employees.jsx
@@ -91,14 +91,25 @@ const Employees = () => {
   ]);
   return (
     <main className="container">
-      <div class="row">
+      <div class="row justify-content-center mt-3 mb-3">
         <div class="col-8">
-          {employees.map((e) => (
-            <div id={e.id} className="card">
-              <img src={femaleProfile} className="card--img--top" alt="" />
-              <div className="card-name"></div>
-            </div>
-          ))}
+          <div class="card--collection">
+            {employees.map((e) => (
+              <div id={e.id} className="card m-2" style={{ cursor: "pointer" }}>
+                {e.gender === "female" ? (
+                  <img src={femaleProfile} className="card--img--top" alt="" />
+                ) : (
+                  <img src={maleProfile} className="card--img--top" alt="" />
+                )}
+                <div className="card--body" class="p-2">
+                  <h5 className="card--title">Fullname: {e.fullName}</h5>
+                  <p className="card--text">
+                    <b>Designation: </b> {e.designation}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </main>
